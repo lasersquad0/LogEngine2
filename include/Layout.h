ce8561f1d8504145dda03a1cd309e6080aab3c49
lasersquad0 <lasersquad@gmail.com>
2025-06-03 13:30:02 +0300

    common Layout methods moved from PatternLayout to Layout class

--- include/Layout.h
+++ include/Layout.h
@@ -15,24 +15,32 @@
 
 LOGENGINE_NS_BEGIN
 
-/*
+
 class Layout
 {
 public:
 	virtual ~Layout() { }
 	virtual std::string Format(const LogEvent& event) = 0;
 
-	virtual void SetPattern(const std::string& , Levels::LogLevel ) { }
-	virtual void SetCritPattern (const std::string& ) { }
-	virtual void SetErrorPattern(const std::string& ) { }
-	virtual void SetWarnPattern (const std::string& ) { }
-	virtual void SetInfoPattern (const std::string& ) { }
-	virtual void SetDebugPattern(const std::string& ) { }
-	virtual void SetTracePattern(const std::string& ) { }
+	virtual void SetPattern(const std::string&, Levels::LogLevel) = 0;
+	virtual void SetPattern(const std::string&) = 0;
+	
+	virtual std::string GetPattern(Levels::LogLevel level) = 0;
+	virtual std::string GetPattern() = 0;
+
+	virtual void SetCritPattern (const std::string& pattern) { SetPattern(pattern, Levels::llCritical); }
+	virtual void SetErrorPattern(const std::string& pattern) { SetPattern(pattern, Levels::llError);    }
+	virtual void SetWarnPattern (const std::string& pattern) { SetPattern(pattern, Levels::llWarning);  }
+	virtual void SetInfoPattern (const std::string& pattern) { SetPattern(pattern, Levels::llInfo);     }
+	virtual void SetDebugPattern(const std::string& pattern) { SetPattern(pattern, Levels::llDebug);    }
+	virtual void SetTracePattern(const std::string& pattern) { SetPattern(pattern, Levels::llTrace);    }
+
+	virtual void SetDefaultPattern(Levels::LogLevel level) = 0;
+	virtual void SetDefaultPattern() = 0;
 
 };
 
-
+/*
 class FixedLayout : public Layout
 {
 public:
